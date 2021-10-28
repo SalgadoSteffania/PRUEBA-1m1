ee79b54b2985762e6b9b3a7c160630efccce34eb
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2021-10-28 09:23:27 -0600

    Add files via upload

---
+++ TareaProgramacion/Formulario/FrmRegistrar.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace TareaProgramacion.Formulario
+{
+    partial class FrmRegistrar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtValorResidual = new System.Windows.Forms.TextBox();
+            this.txtValorActivo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbTipoActivo = new System.Windows.Forms.ComboBox();
+            this.cmbMetodoDepreciacion = new System.Windows.Forms.ComboBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(105, 41);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(105, 90);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtDescripcion.TabIndex = 1;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(105, 142);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // txtValorResidual
+            // 
+            this.txtValorResidual.Location = new System.Drawing.Point(105, 259);
+            this.txtValorResidual.Name = "txtValorResidual";
+            this.txtValorResidual.Size = new System.Drawing.Size(100, 20);
+            this.txtValorResidual.TabIndex = 4;
+            // 
+            // txtValorActivo
+            // 
+            this.txtValorActivo.Location = new System.Drawing.Point(105, 196);
+            this.txtValorActivo.Name = "txtValorActivo";
+            this.txtValorActivo.Size = new System.Drawing.Size(100, 20);
+            this.txtValorActivo.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Valor residual";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Valor activo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Codigo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Descripcion";
+            // 
+            // cmbTipoActivo
+            // 
+            this.cmbTipoActivo.FormattingEnabled = true;
+            this.cmbTipoActivo.Location = new System.Drawing.Point(105, 317);
+            this.cmbTipoActivo.Name = "cmbTipoActivo";
+            this.cmbTipoActivo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipoActivo.TabIndex = 11;
+            this.cmbTipoActivo.SelectedIndexChanged += new System.EventHandler(this.CmbTipoActivo_SelectedIndexChanged);
+            // 
+            // cmbMetodoDepreciacion
+            // 
+            this.cmbMetodoDepreciacion.FormattingEnabled = true;
+            this.cmbMetodoDepreciacion.Location = new System.Drawing.Point(130, 395);
+            this.cmbMetodoDepreciacion.Name = "cmbMetodoDepreciacion";
+            this.cmbMetodoDepreciacion.Size = new System.Drawing.Size(121, 21);
+            this.cmbMetodoDepreciacion.TabIndex = 12;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(130, 453);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 13;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.BtnEnviar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 317);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Tipo activo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 398);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Tipo depreciacion";
+            // 
+            // FrmRegistrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 488);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.cmbMetodoDepreciacion);
+            this.Controls.Add(this.cmbTipoActivo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValorActivo);
+            this.Controls.Add(this.txtValorResidual);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "FrmRegistrar";
+            this.Text = "FrmRegistrar";
+            this.Load += new System.EventHandler(this.FrmRegistrar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtValorResidual;
+        private System.Windows.Forms.TextBox txtValorActivo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbTipoActivo;
+        private System.Windows.Forms.ComboBox cmbMetodoDepreciacion;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ TareaProgramacion/Formulario/FrmRegistrar.cs
@@ -0,0 +1,46 @@
+﻿using AppCore.Interfaces.Services;
+using Domain.ActivoFijo.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TareaProgramacion.Formulario
+{
+    public partial class FrmRegistrar : Form
+    {
+        private ActivoFijoServices Activos;
+        
+        public FrmRegistrar(ActivoFijoServices activos)
+        {
+            this.Activos = activos;
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmbTipoActivo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FrmRegistrar_Load(object sender, EventArgs e)
+        {
+            cmbTipoActivo.Items.AddRange(Enum.GetValues(typeof(EnumsTipoActivo)).Cast<object>().ToArray());
+            cmbMetodoDepreciacion.Items.AddRange(Enum.GetValues(typeof(EnumsTipoActivo)).Cast<object>().ToArray());
+        }
+
+        private void BtnEnviar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
