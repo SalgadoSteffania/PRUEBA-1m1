b765f304f20b019a6dc7e393443c096f83533496
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2021-10-28 09:24:09 -0600

    Add files via upload

---
+++ Domain/ActivoFijo/ActivoFijo.cs
@@ -0,0 +1,38 @@
+﻿using Domain.ActivoFijo.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ActivoFijo
+{
+    public class ActivoFijo
+    {
+
+        public string Nombre { get; set; }
+        public int Id { get; set; }
+        public string codigo { get; set; }
+        public string descripcion { get; set; }
+
+        public decimal ValorActivo { get; set; }
+
+        public decimal ValorResidual { get; set; }
+        public int VidaUtil { get; set; }
+        public DateTime FechaDeAdquisicion { get; set; }
+
+        public EnumsTipoActivo TipoActivo { get; set; }
+        public Depreciacion depreciacion { get; set; }
+
+        public String toString()
+        {
+
+            return $"Nombre: {Nombre}       codigo: {codigo}    ID: {Id}    descripcion: {descripcion}      Vida Util: {VidaUtil}  " +
+                $" Depreciacion: {Environment.NewLine}";
+        }
+
+
+
+
+    }
+}
---
+++ Domain/ActivoFijo/Enums/EnumsTipoActivo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ActivoFijo.Enums
+{
+    public enum EnumsTipoActivo
+    {
+        Edificio,
+        Vehiculo,
+        Maquinaria,
+        Mobiliaria,
+        EquipoComputo,
+
+
+
+    }
+}
